1be315065b55fb2623f02611a37c80a881a2cc8f
・ω・（竹子） <you948712@gmail.com>
2025-02-09 07:10:44 +0800

    fix: button

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ class Plugin {
     const focusButton = document.querySelector("#focus");
     if (focusButton) {
       const button = document.createElement("div");
-      button.id = "radar";
+      button.id = "image";
       button.className = "nav-bar-location";
       button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-560H200v560Zm40-80h480L570-480 450-320l-90-120-120 160Zm-40 80v-560 560Z"/></svg>`;
       focusButton.insertAdjacentElement("afterend", button);
@@ -19,10 +19,10 @@ class Plugin {
 
   addClickEvent() {
     const { info } = this.#ctx;
-    const button = document.querySelector("#radar");
+    const button = document.querySelector("#image");
     button.addEventListener("click", () => {
       ipcRenderer.send("open-plugin-window", {
-          pluginId: "radar",
+          pluginId: "image",
           htmlPath: `${info.pluginDir}/image/web/image.html`,
           options: {
             width          : 870,
